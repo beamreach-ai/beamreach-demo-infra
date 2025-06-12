78e70e58ce146d99a0ebca97eacfb04299701460
Beamreach Radio <radio@beamreach.ai>
2025-06-12 09:51:47 +0000

    [Beamreach] Add read-only S3 permissions to the role by attaching the AWS managed policy 'AmazonS3ReadOnlyAccess' or creating an inline policy with s3:GetObject, s3:GetObjectVersion, s3:ListBucket, and s3:ListAllMyBuckets permissions for all S3 resources

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -17,7 +17,7 @@ resource "aws_iam_role" "demo123" {
 
 resource "aws_iam_role_policy" "demo123_policy" {
   name = "demo123-policy"
-  role = aws_iam_role.github_radio_user.id
+  role = aws_iam_role.demo123.id
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -32,6 +32,16 @@ resource "aws_iam_role_policy" "demo123_policy" {
           "ecr:UploadLayerPart",
           "ecr:CompleteLayerUpload",
           "ecr:DescribeRepositories"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:ListBucket",
+          "s3:ListAllMyBuckets"
         ],
         Resource = "*"
       }
