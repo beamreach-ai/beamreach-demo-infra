f6f7c9b8a70ca8303dc293f9c309433755b45564
Ilia Dubovskii <108252420+beamreachcloud@users.noreply.github.com>
2025-10-28 12:13:30 +0000

    Fix S3 bucket public ACL blocking for insecure_bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_ownership_controls" "insecure_controls" {
 
 resource "aws_s3_bucket_public_access_block" "insecure_block" {
   bucket                  = aws_s3_bucket.insecure_bucket.id
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
