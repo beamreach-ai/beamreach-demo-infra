62a30c6a3d1398fd362e4b3332c5b236c7d7dd88
Ilia Dubovskii <108252420+beamreachcloud@users.noreply.github.com>
2025-10-28 22:17:52 +0000

    Fix AVD-AWS-0086: Enable block_public_acls for S3 bucket public access block

--- modules/prowler_findings/main.tf
+++ modules/prowler_findings/main.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "public_demo" {
 
 resource "aws_s3_bucket_public_access_block" "public_demo" {
   bucket                  = aws_s3_bucket.public_demo.id
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
